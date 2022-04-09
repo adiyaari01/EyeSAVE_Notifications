1e7eff2cfedabdef5b94eb6e9211e7cd6588afa1
Adi Yaari <ayaari@aligntech.com>
2022-04-09 14:14:00 +0300

    fix bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const mongoose = require("mongoose");
 require('dotenv').config()
 const path = require('path')
+const cors = require('cors')
 
 const app = express();
 // const port = 8001;
