191b2d8162f8ca2b23da0fab4d5b71f2f41aa5a7
Adi Yaari <ayaari@aligntech.com>
2022-04-09 14:24:57 +0300

    return answer

--- controller.js
+++ controller.js
@@ -1,24 +1,28 @@
-const {sendMessageToParent} = require("./telegram/parentsBot");
-const {sendMessageToStaff} = require("./telegram/staffBot");
+const { sendMessageToParent } = require("./telegram/parentsBot");
+const { sendMessageToStaff } = require("./telegram/staffBot");
 
-exports.sendNotificationToParent = async (req,res,next)=>{
-  await sendMessageToParent(req.body.userId,req.body.msg);
-  return res.status(200);
+exports.sendNotificationToParent = async (req, res, next) => {
+  try {
+    await sendMessageToParent(req.body.userId, req.body.msg);
+    return res.json({success: true});
+  } catch (error) {
+    return res.sendStatus(500);
+  }
 };
 
-exports.sendNotificationToParents = async (req,res,next)=>{
-  await Promise.all(req.body.parents.map(parentId=>{
+exports.sendNotificationToParents = async (req, res, next) => {
+  await Promise.all(req.body.parents.map(parentId => {
     return sendMessageToParent(parentId, req.body.msg);
   }))
   return res.status(200);
 };
 
-exports.sendNotificationToEmployee = async (req,res,next)=>{
-  await sendMessageToStaff(req.body.userId,req.body.msg);
+exports.sendNotificationToEmployee = async (req, res, next) => {
+  await sendMessageToStaff(req.body.userId, req.body.msg);
   return res.status(200);
 };
-exports.sendNotificationToStaff = async (req,res,next)=>{
-  await Promise.all(req.body.staff.map(employeeId=>{
+exports.sendNotificationToStaff = async (req, res, next) => {
+  await Promise.all(req.body.staff.map(employeeId => {
     return sendMessageToStaff(employeeId, req.body.msg);
   }))
   return res.status(200);
