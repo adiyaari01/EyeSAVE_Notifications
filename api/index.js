f2de1b3ce2bcd73c670684835719fec96b425372
Adi Yaari <ayaari@aligntech.com>
2022-04-17 21:43:14 +0300

    change url

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const BASE_URL =
-  process.env.NODE_ENV === "production" ? "https://eyesaveserver.herokuapp.com" : "http://localhost:8000";
+  process.env.NODE_ENV === "production" ? "https://eyesave.herokuapp.com" : "http://localhost:8000";
 
 exports.getRequest = suffix =>axios.get(`${BASE_URL}/${suffix}`);
 exports.updateRequest = (suffix, body) =>axios.put(`${BASE_URL}/${suffix}`,body);
