01c291ecfe0063059902a07e036760a2571ca6ab
adiyaari01 <adiyaari01@gmail.com>
2022-06-17 20:22:43 +0300

    fix cors

--- socket.js
+++ socket.js
@@ -2,14 +2,15 @@ const { Server } = require("socket.io");
 let io;
 module.exports.init = (server) => {
   io = new Server(server,{
-    cors: {
-        origin: ['*']
-        // [    
-        //   "http://127.0.0.1:3000",
-        //   "http://localhost:3000",
-        //   "https://eyesave.netlify.app",
-        // ]
-    }
+    // cors: {
+    //     origin: ['*']
+    //     // [    
+    //     //   "http://127.0.0.1:3000",
+    //     //   "http://localhost:3000",
+    //     //   "https://eyesave.netlify.app",
+    //     // ]
+    // },
+    allowRequest: (req,callback) => callback(null,true)
   });
 };
 
