3f85db23963a0c8a86e1e6b4d0da0afd38c3006f
adiyaari01 <adiyaari01@gmail.com>
2022-06-17 20:09:04 +0300

    cors

--- socket.js
+++ socket.js
@@ -3,11 +3,12 @@ let io;
 module.exports.init = (server) => {
   io = new Server(server,{
     cors: {
-        origin: [    
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "https://eyesave.netlify.app",
-      ]
+        origin: true
+        // [    
+        //   "http://127.0.0.1:3000",
+        //   "http://localhost:3000",
+        //   "https://eyesave.netlify.app",
+        // ]
     }
   });
 };
