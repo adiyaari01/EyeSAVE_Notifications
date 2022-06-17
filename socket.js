100d4ea3ce6ca59b71afb6e39f8b5b587969109f
adiyaari01 <adiyaari01@gmail.com>
2022-06-17 20:26:49 +0300

    cors

--- socket.js
+++ socket.js
@@ -1,16 +1,18 @@
 const { Server } = require("socket.io");
 let io;
 module.exports.init = (server) => {
-  io = new Server(server,{
-    // cors: {
-    //     origin: ['*']
-    //     // [    
-    //     //   "http://127.0.0.1:3000",
-    //     //   "http://localhost:3000",
-    //     //   "https://eyesave.netlify.app",
-    //     // ]
-    // },
-    allowRequest: (req,callback) => callback(null,true)
+  io = new Server(server, {
+    cors: {
+      origin: [
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "https://eyesave.netlify.app",
+        "https://eyesave.herokuapp.com",
+        "https://eye-save-noitfications.herokuapp.com",
+      ],
+      credentials: true,
+      optionSuccessStatus: 200,
+    },
   });
 };
 
