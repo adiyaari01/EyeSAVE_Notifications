81d5460156f01ff1ae8b81a4d51012ad4ec10cf2
adiyaari01 <adiyaari01@gmail.com>
2022-06-17 20:29:05 +0300

    cors

--- socket.js
+++ socket.js
@@ -4,12 +4,13 @@ module.exports.init = (server) => {
   io = new Server(server, {
     cors: {
       origin: [
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "https://eyesave.netlify.app",
-        "https://eyesave.herokuapp.com",
-        "https://eye-save-noitfications.herokuapp.com",
+        "http://127.0.0.1:3000/",
+        "http://localhost:3000/",
+        "https://eyesave.netlify.app/",
+        "https://eyesave.herokuapp.com/",
+        "https://eye-save-noitfications.herokuapp.com/",
       ],
+      methods: ["GET","POST"],
       credentials: true,
       optionSuccessStatus: 200,
     },
